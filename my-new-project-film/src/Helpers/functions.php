336ee40e536acfc8857d7d8ff2b3681c7c2d2cd3
Diana <dv870906@gmail.com>
2026-04-16 22:45:52 +0700

    удалили проект

--- my-new-project-film/src/Helpers/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Простая защита от XSS
- * Использование: вместо echo $var пиши echo e($var)
- */
-function e($text) {
-    return htmlspecialchars($text ?? '', ENT_QUOTES, 'UTF-8');
-}
-?>
